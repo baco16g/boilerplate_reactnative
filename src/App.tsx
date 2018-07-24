8d01e47304b7b0000e23a0adbed91894c07d0332
baco16g <baco.16g@gmail.com>
2018-07-24 13:23:19 +0900

    chore(*): add tslint

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,8 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu'
 })
 
-type Props = {}
-export default class App extends React.Component<Props> {
-  render() {
+export default class App extends React.Component<{}> {
+  public render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
